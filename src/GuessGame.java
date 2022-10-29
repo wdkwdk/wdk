f78059756bab9ea0ad5394b56864589d927359e8
wdk <2031557931@qq.com>
2022-10-29 19:19:22 +0800

    Add a guessgame

---
+++ src/GuessGame.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+public class GuessGame {
+    public static void main(String[] args) {
+        int number = (int)(Math.random()*100);
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            System.out.println("Guess a number between 0 and 100");
+            int guessNumber = sc.nextInt();
+            if (number == guessNumber) {
+            System.out.println("WIN!!");
+                break;
+            } else if (number < guessNumber) {
+                System.out.println("BIGGER");
+            } else if (number > guessNumber) {
+                System.out.println("SMALLER");
+            }else {
+                System.out.println("Please guess a number between 0 and 100");
+            }
+        }
+
+
+    }
+}
